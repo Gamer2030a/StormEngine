496f6da6599e75a92b7284e09b765b8a4128a27a
Parsa Dehghani <90768871+Gamer2030a@users.noreply.github.com>
2021-11-21 02:44:03 +0330

    Delete Storm directory

--- Storm/SRC/Storm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// for use by Storm applications
-
-#include "Storm/Application.h"
-#include "Storm/Log.h"
-
-//Entry Point
-#include "Storm/EntryPoint.h"
--- Storm/SRC/Storm/FMath.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void Fastinversesquareroot(float number)
-{
-
-}
-
